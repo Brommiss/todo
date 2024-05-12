ea1bbdd01ba797c3634d1d8a1f2c9981675d7861
Brommiss <bromm@local.gay>
2024-05-12 13:09:32 +0300

    cock

--- src/Tests.ts
+++ src/Tests.ts
@@ -1,25 +1,26 @@
-// function greet() {
-//   return 'hello world!'
-// }
+function greet() {
+  return 'hello world!'
+}
 
-// const a = 'sex in a big city'
-// reverseWord(a)
-// function reverseWord(a) {
-//   return a.split(' ').map((x) => x.split('').reverse().join('')).join(' ')
-// }
+const a = 'sex in a big city'
+reverseWord(a)
+function reverseWord(a) {
+  return a.split(' ').map((x) => x.split('').reverse().join('')).join(' ')
+}
 
-// function makeNegative(num) {
-//   return (num <= 0) ? num : -num
-// }
+console.log(makeNegative(20))
+function makeNegative(num: number): number {
+  return (num <= 0) ? num : -num
+}
 
-// console.log(past(1, 4, 2))
-// function past(h, m, s) {
-//   return h * 36e5 + m * 6e4 + s * 1e3
-// }
-//
-// busStops([[10, 0], [3, 5], [5, 8]])
-// function busStops(key) {
-//   const ms = key.flatMap((inKey) => inKey[0] - inKey[1])
-//   const summ = ms.reduce((sum, m) => sum + m, 0)
-//   return summ
-// }
+console.log(past(1, 4, 2))
+function past(h, m, s) {
+  return h * 36e5 + m * 6e4 + s * 1e3
+}
+
+busStops([[10, 0], [3, 5], [5, 8]])
+function busStops(key) {
+  const ms = key.flatMap((inKey) => inKey[0] - inKey[1])
+  const summ = ms.reduce((sum, m) => sum + m, 0)
+  return summ
+}
